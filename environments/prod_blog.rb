c2338cb81e67b2224670bbf30fc1c451b772b79c
Alex Pop <al3xpop@gmail.com>
2014-10-22 01:52:54 +0100

    pin cookbook versions in hash

--- environments/prod_blog.rb
+++ environments/prod_blog.rb
@@ -1,12 +1,14 @@
 name "prod_blog"
 description "Prod blog environment"
 
-cookbook "haproxy", "= 1.6.6"
-cookbook "myapache-cookbook", "= 0.2.10"
+cookbook_versions({
+  "haproxy"=>"= 1.6.6",
+  "myapache-cookbook"=>"= 0.2.10"
+})
 
-override_attributes(
-    'myapache-cookbook' => {
-             'git-repo' => 'https://github.com/alexpop/myapache-app',
-             'git-revision' => '76bbc80189b0dd2341035da28140a08697d9a507' }
-)
+override_attributes({
+  'myapache-cookbook' => {
+           'git-repo' => 'https://github.com/alexpop/myapache-app',
+           'git-revision' => '76bbc80189b0dd2341035da28140a08697d9a507' }
+})
 
--- environments/test_blog.rb
+++ environments/test_blog.rb
@@ -1,11 +1,13 @@
 name "test_blog"
 description "Test blog environment"
 
-cookbook "haproxy", "= 1.6.6"
-cookbook "myapache-cookbook", "= 0.2.10"
+cookbook_versions({
+	  "haproxy"=>"= 1.6.6",
+		  "myapache-cookbook"=>"= 0.2.10"
+})
 
-override_attributes(
-    'myapache-cookbook' => {
-             'git-repo' => 'https://github.com/alexpop/myapache-app',
-						 'git-revision' => 'b61978bf02116fde8b09cf62a0540ab6c6d08ada' }
-)
+override_attributes({
+  'myapache-cookbook' => {
+           'git-repo' => 'https://github.com/alexpop/myapache-app',
+           'git-revision' => 'b61978bf02116fde8b09cf62a0540ab6c6d08ada' }
+})
