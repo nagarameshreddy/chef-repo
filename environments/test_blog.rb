2c0106f487b7f031803cc4e2ed44f9580220f1b6
Alex Pop <al3xpop@gmail.com>
2014-12-17 14:59:49 +0000

    env change

--- environments/test_blog.rb
+++ environments/test_blog.rb
@@ -2,8 +2,8 @@ name 'test_blog'
 description 'Test blog environment'
 
 cookbook_versions({
-  'haproxy'=>'= 1.6.6',
-  'myapache-cookbook'=>'= 0.2.10'
+  'haproxy'=>'>= 1.6.6',
+  'myapache-cookbook'=>'>= 0.2.10'
 })
 
 override_attributes({
