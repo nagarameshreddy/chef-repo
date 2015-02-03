87a7568d46a0e6b4c9490c8c9ba9939812ae9333
Alex Pop <al3xpop@gmail.com>
2015-02-03 10:32:15 +0100

    json environments

--- roles/linux_apache.rb
+++ roles/linux_apache.rb
@@ -1,4 +1,5 @@
 name "linux_apache"
 description "Linux apache role"
-run_list "recipe[myapache-cookbook::default]", 
+run_list "role[linux_base]",
+         "recipe[myapache-cookbook::default]", 
          "recipe[myapache-cookbook::app-checkout]"
