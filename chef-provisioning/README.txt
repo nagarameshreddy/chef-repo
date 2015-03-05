a9521c28d7712e1aefb3f0b83f7c69a6e915d324
Alex Pop <al3xpop@gmail.com>
2015-03-05 16:25:04 +0000

    provisioning changes

--- chef-provisioning/README.txt
+++ chef-provisioning/README.txt
@@ -1,3 +1,8 @@
+# Stay on the latest gems to benefit from latest features and bug fixes
+chef gem list chef-provisioning
+chef gem install chef-provisioning
+chef gem install chef-provisioning-aws
+
 # Run chef-provisioning to create AWS ELB and instances using chef-provisioning
 export CHEF_DRIVER=aws
 cd hosted_aws_demo
--- chef-provisioning/hosted_aws_demo/apache_demo.rb
+++ chef-provisioning/hosted_aws_demo/apache_demo.rb
@@ -1,14 +1,25 @@
 require 'chef/provisioning/aws_driver'
 
+# Specify the driver to be used here
+with_driver 'aws'
+
+# Enable Chef server communication
 config_file = "/etc/chef/knife.rb"
 Chef::Config.from_file(config_file)
-
 with_chef_server Chef::Config[:chef_server_url], {
   :client_name => Chef::Config[:node_name],
   :signing_key_filename => Chef::Config[:client_key]
 }
 
-log "*** Using chef environment: #{node['provision']['chef']['environment']}"
+log "*** Creating chef environment: #{node['provision']['name_prefix']}"
+
+# Create/Destroy the Chef environment for the nodes
+chef_environment node['provision']['name_prefix'] do
+  default_attributes node['provision']['default_attributes']
+  description "Auto-generated using chef-provisioning"
+  complete true
+  action (node['provision']['all']=="true" ? :create : :delete)
+end
 
 # Create an array of machine names
 the_machines = []
@@ -16,12 +27,14 @@ for i in (1..node['provision']['instances'].to_i)
   the_machines.push("#{node['provision']['name_prefix']}-#{i}")
 end
 
+# Create/Destroy machines in parallel
 machine_batch do
   the_machines.each do |machine_name|
     machine machine_name do
       tag "name=#{machine_name}"
       run_list node['provision']['chef']['run_list'].split(/\s*,\s*/)
-      chef_environment node['provision']['chef']['environment']
+      chef_environment node['provision']['name_prefix']
+      converge true
       machine_options({
         :ssh_username => 'root',
         :bootstrap_options => {
@@ -34,10 +47,10 @@ machine_batch do
       })
     end
   end
-  # other actions :converge, :destroy, :nothing
   action (node['provision']['all']=="true" ? :converge : :destroy)
 end
 
+# Create/Destroy an AWS ELB used as an HTTP load balancer for the machines
 load_balancer "#{node['provision']['name_prefix']}-elb" do
   load_balancer_options :availability_zones => [node['provision']['cloud']['availability_zone']],
     :listeners => [{
