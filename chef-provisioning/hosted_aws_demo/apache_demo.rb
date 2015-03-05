4367103fa96c9563f69a2a850f88a6c473f9907d
Alex Pop <al3xpop@gmail.com>
2015-03-05 18:01:23 +0000

    basic recipe description

--- chef-provisioning/hosted_aws_demo/apache_demo.rb
+++ chef-provisioning/hosted_aws_demo/apache_demo.rb
@@ -1,3 +1,10 @@
+#
+# chef-provisioning recipe to create AWS EC2 instances behind an HTTP ELB
+# Required attributes are stored in ../attrs.json
+# In the json file, set node['provision']['all'] to "true" to 
+# create the environment and "false" to destroy it
+#
+
 require 'chef/provisioning/aws_driver'
 
 # Specify the driver to be used here
